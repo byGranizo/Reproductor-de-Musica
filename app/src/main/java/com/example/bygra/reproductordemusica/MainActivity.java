a1340d03e074fe04d9fd5376737a91fe75a5dea5
Miguel Jimenez Granizo <migueljg1997@gmail.com>
2018-11-11 11:38:44 +0100

    .

--- app/src/main/java/com/example/bygra/reproductordemusica/MainActivity.java
+++ app/src/main/java/com/example/bygra/reproductordemusica/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         File carpetaMusica = new File(getExternalStorageDirectory(), "musica");
         carpetaMusica.mkdirs();
 
-        mpList.add(MediaPlayer.create(getApplicationContext(), R.raw.evilmortytheme));
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.la_raiz_suya_es_mi_guerra.mp3);
 
 
     }
